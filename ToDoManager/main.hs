b93cf8f11220cb114cd381b6c4279e6ef2781c4c
Kaiakanj1 <137309460+Kaiakanj1@users.noreply.github.com>
2025-11-01 10:37:57 -0400

    modified to use file IO, also added quit option

--- ToDoManager/main.hs
+++ ToDoManager/main.hs
@@ -6,56 +6,59 @@ import System.IO
 printTasks :: [String] -> IO ()
 printTasks [] = return ()
 printTasks (x:xs) = do
-  putStrLn (" " ++ x)
-  printTasks xs
+    putStrLn (" " ++ x)
+    printTasks xs
 
 -- Recursively search for a task
 searchTasks :: String -> [String] -> Bool
 searchTasks _ [] = False
 searchTasks q (x:xs) =
-  if q == x
-    then True
-    else searchTasks q xs  
+    if q == x
+        then True
+        else searchTasks q xs  
 
 main :: IO ()
 main = do
-  putStrLn "Welcome to Task Manager 3000"
-  menu []
+    putStrLn "Welcome to Task Manager 3000"
+    menu []
 
 menu :: [String] -> IO ()
 menu tasks = do
-  putStrLn "Below are the options:"
-  putStrLn " add"
-  putStrLn " print"
-  putStrLn " search"
-  putStr "Enter option:\n"
-  hFlush stdout
-  option <- getLine
+    putStrLn "Below are the options:"
+    putStrLn " add"
+    putStrLn " print"
+    putStrLn " search"
+    putStrLn " quit"
+    putStr "Enter option:\n"
+    hFlush stdout
+    option <- getLine
 
-  if option == "add" || option == "Add" -- added in both upper and lower case incase user inputs either 
-    then do
-      putStr "Enter Task to Add:\n"
-      hFlush stdout
-      newTask <- getLine
-      let newList = newTask : tasks
-      menu newList
+    if option == "add" || option == "Add" then do
+        putStr "Enter Task to Add:\n"
+        hFlush stdout
+        newTask <- getLine
+        let newList = newTask : tasks
+        -- Save tasks to file (creates or overwrites "tasks.txt")
+        writeFile "tasks.txt" (unlines newList)
+        menu newList
 
-    else if option == "print" || option == "Print"
-      then do
+    else if option == "print" || option == "Print" then do
         putStrLn "Here are your tasks:"
         printTasks tasks
         menu tasks
 
-      else if option == "search" || option == "Search"
-        then do
-          putStr "Enter Task to Search:\n"
-          hFlush stdout
-          query <- getLine
-          if searchTasks query tasks
+    else if option == "search" || option == "Search" then do
+        putStr "Enter Task to Search:\n"
+        hFlush stdout
+        query <- getLine
+        if searchTasks query tasks
             then putStrLn ("Found " ++ query)
             else putStrLn ("Could not find " ++ query)
-          menu tasks
+        menu tasks
 
-        else do
-          putStrLn "Error"
-          menu tasks
+    else if option == "quit" || option == "Quit" then do -- added option to exit program
+        putStrLn "Goodbye!"
+
+    else do
+        putStrLn "Error"
+        menu tasks
