5add03db5e875e6905cc9892a0e7e1053cdb48b9
Kaiakanj1 <137309460+Kaiakanj1@users.noreply.github.com>
2025-11-12 15:55:34 -0500

    Moved manager.pl to it's own folder. Finished Remaining tasks. Added interaction.txt

--- DigitalAdvisor/manager.pl
+++ DigitalAdvisor/manager.pl
@@ -11,15 +11,15 @@ breadth(4, [csds313, csds335, csds340, csds391]).
 
 security([csds344, csds356, csds427, csds444, csds448]). 
 
-% TODO: need to make these accurate later 
+% Computer Science technical electives
 elective([csds301, csds303, csds315, csds317, csds419, csds484, csds431, csds490, csds491, csds492]).
 
 % this portion of the code lists out prereqs for the courses
 prereq(csds132, []).
 prereq(csds233, [csds132]).
 prereq(csds281, [csds132]).
-prereq(csds302, [math122]).
-prereq(csds310, [csds302, csds233]).
+prereq(csds302, []).
+prereq(csds310, [csds302, csds233]). 
 prereq(csds395, []).
 
 % these are courses that require a language 
@@ -62,29 +62,42 @@ menu :-
     write('    3. Check Prerequisites'), nl,
     write('    4. Check Courses for Language'), nl,
     write('    Anything Else. Quit'), nl,
-    write('    Enter Choice: '),
+    write('Enter Choice: '),
     read(Choice),
     handler(Choice). 
 	
 	handler(1) :- 
     	write('Core Courses'), nl,
     	corecourses(C), print_list(C), nl,
-    	breadth(N, List),
-    	print_list(List),
+        forall(breadth(N, List),
+           (format('Breadth Area ~w Courses (Choose 2)~n', [N]),
+            print_list(List), nl)),
     	write('Security Course (Choose 1)'), nl,
-		security(S), print_list(S). 
+		security(S), print_list(S), nl, nl,
+        menu. % loop back to menu 
 	handler(2) :-
     	write('Enter Elective Course'), 
     	read(C), 
-		is_elective(C).
+		is_elective(C), nl, nl, 
+        menu.
 	handler(3) :-
     	write('Enter Course to get prereqs'),
     	read(C),
-    	is_prereq(C).
+    	is_prereq(C), nl, nl,
+        menu.
 	handler(4) :-
-		write('Enter Language'),
-		read(C),
-		course_language(X, C). 
+        write('Enter language: '), nl,
+        read(Lang),
+        findall(Course, course_language(Course, Lang), Courses),
+        (   Courses = [] ->
+            format('  Sorry, no courses include ~w~n', [Lang])
+        ;   format('  The below courses include ~w~n', [Lang]),
+            print_list(Courses)
+        ),
+        nl, menu.
+
+    handler(_) :- % any other input quits
+        write('Have a good day!'), nl.
     
 
 
